775dfd2bcc1751b5e371b07e3f0fc4219a61c01b
Amir <amirdp8102@gmail.com>
2021-07-27 16:08:13 +0430

    Setup database seeder

--- project/database/seeders/DatabaseSeeder.php
+++ project/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\ProductOption;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +17,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(['email' => 'manager@coffe.malltina', 'type' => 'manager'])->create();
+        User::factory(['email' => 'manager@coffe.malltina', 'type' => 'manager'])->create();
+
+//      Creating product
+        $products = Product::factory(6)->create();
+
+//      Creating some options
+        $options = ProductOption::factory()->create();
+
+//       Registering the final order
+        Order::factory(['product_id' => $products->first()->id, 'options_id' => $options->first()->id])->create();
     }
 }
