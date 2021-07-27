e40a9b9f328247da4009ea63244debd8b7dc4c78
Amir <amirdp8102@gmail.com>
2021-07-27 16:08:53 +0430

    Add an api route to view menu

--- project/routes/api.php
+++ project/routes/api.php
@@ -1,14 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
 
+// Auth
 Route::post('/login', [AuthController::class, 'login'])->name('login.api');
 
+// Manager
 Route::middleware(['auth:sanctum', 'user.type'])->prefix('/manager')->group(function () {
     Route::post('edit-order', function () {
         return "Hello!";
     });
 });
+
+// Customers
+Route::get('/', [ProductController::class, 'index']);
