b8fd52aa3084cce4c15c41f6c49d8d0a8d70672f
Amir <amirdp8102@gmail.com>
2021-07-27 11:29:38 +0430

    Add user.type middleware to manager group

--- project/routes/api.php
+++ project/routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login'])->name('login.api');
 
-Route::middleware('auth:sanctum')->prefix('/manager')->group(function () {
+Route::middleware(['auth:sanctum', 'user.type'])->prefix('/manager')->group(function () {
     Route::post('edit-order', function () {
         return "Hello!";
     });
