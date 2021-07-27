90f35e4b6a6d81d24dba9f07edced7fbc75e80b8
Amir <amirdp8102@gmail.com>
2021-07-27 15:53:45 +0430

    Add orders table

---
+++ project/database/migrations/2021_07_27_072624_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_email')->unique();
+            $table->enum('consume_location', ['in shop', 'take away']);
+            $table->enum('status', ['waiting', 'preparation', 'ready', 'delivered', 'canceled']);
+            $table->bigInteger('product_id');
+            $table->bigInteger('options_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
