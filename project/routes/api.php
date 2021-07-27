cc8d95ef6ffa57622e4d5ac5bb6e7347ff9bcc8a
Amir <amirdp8102@gmail.com>
2021-07-27 10:49:33 +0430

    Add api login route

--- project/routes/api.php
+++ project/routes/api.php
@@ -3,17 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\AuthController;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [AuthController::class, 'login'])->name('login.api');
+
+Route::middleware('auth:sanctum')->prefix('/manager')->group(function () {
+    Route::post('edit-order', function () {
+        return "Hello!";
+    });
 });
